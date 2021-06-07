a9412831e5da49e8c77bc9995ee3512f3424f88c
msimbo-coding <SAlmonte@MSIMBO.org>
2021-06-07 18:33:19 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,10 @@ module.exports = {
             transform: "translate(0px, 0px) scale(1)"
           },
           "33%": {
-            transform: "translate(30px, -50px) scale(1.2)"
+            transform: "translate(30px, -50px) scale(1.1)"
           },
           "66%": {
-            transform: "translate(20px, -20px) scale(0.8)"
+            transform: "translate(20px, -20px) scale(0.9)"
           },
           "100%": {
             transform: "translate(0px, 0px) scale(1)"

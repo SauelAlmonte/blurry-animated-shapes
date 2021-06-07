f4486aa09f8b1a2e52098876482253b3881d85e0
msimbo-coding <SAlmonte@MSIMBO.org>
2021-06-07 18:18:56 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,59 +1,11 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      animation: {
-        blob: "blob 4s infinite",
-      }
-      keyframes: {
-        blob: {
-          "0%": {
-            transform: "scale(1)",
-          },
-          "33%": {
-            transform:  "scale(1.1)",
-          },
-          "66%": {
-            transform:  "scale(0.9)",
-          },
-          "100%": {
-            transform:  "scale(1)",
-          },
-        },
-      },
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'blue-gray': colors.blueGray,
-        'cool-gray': colors.coolGray,
-        gray: colors.gray,
-        'true-gray': colors.trueGray,
-        'warm-gray': colors.warmGray,
-        red: colors.red,
-        orange: colors.orange,
-        amber: colors.amber,
-        yellow: colors.yellow,
-        lime: colors.lime,
-        green: colors.green,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        'light-blue': colors.lightBlue,
-        blue: colors.blue,
-        indigo: colors.indigo,
-        violet: colors.violet,
-        purple: colors.purple,
-        fuchsia: colors.fuchsia,
-        pink: colors.pink,
-        rose: colors.rose,
-      }
-    },
+    extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};

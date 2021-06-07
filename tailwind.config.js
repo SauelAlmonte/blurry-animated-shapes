93139ba5f4911fae356876d428cb2ca2babceb60
msimbo-coding <SAlmonte@MSIMBO.org>
2021-06-07 18:27:23 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "scale(1)"
+          },
+          "33%": {
+            transform: "scale(1.1)"
+          },
+          "66%": {
+            transform: "scale(0.9)"
+          },
+          "100%": {
+            transform: "scale(1)"
+          },
+        },
+      },
+      animation: {
+        blob: "blob 4s infinite"
+      },
+    },
   },
   variants: {
     extend: {},
